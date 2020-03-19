3b0700d25fe552bba973c1f552b09958fdeba2c9
astroboi <terry007x@korea.ac.kr>
2020-03-19 19:32:17 +0900

    주석 추가

--- Main.py
+++ Main.py
@@ -7,24 +7,24 @@ import Process
 WEB_DRV_PATH = "C:/Users/terry/chromedriver.exe"
 PDF_PATH = "C:/Users/terry/Documents/카카오톡 받은 파일/2020-1_Korea_Uni/신입생 연락처-최종.pdf"
 """
+원우회사이트정보.txt 내용
 url
 id
 passwrd
 """
 TAGET_INFO = "C:/Users/terry/Documents/카카오톡 받은 파일/2020-1_Korea_Uni/원우회사이트정보.txt"
-GISU = "65"
+GISU = "65" # 신입생 기수
 ############### end setting env ################
 WEB_DRV = webdriver.Chrome(WEB_DRV_PATH)
 def main():
     util = Utils.Utils()
+    table_data = util.read_pdf_as_table(PDF_PATH,False)
     target_info_list = util.read_txt_to_list(TAGET_INFO)
     target_url = target_info_list[0]
     target_id = target_info_list[1]
     target_pwd = target_info_list[2]
 
     process = Process.Process(WEB_DRV,target_url)
-
-    table_data = util.read_pdf_as_table(PDF_PATH,False)
 
     process.go_to_url(target_url + "/Admin/")
     login_elements = [
